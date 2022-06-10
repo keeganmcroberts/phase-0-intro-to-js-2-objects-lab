ded3ed8f6370b3e7b89d20a17e67e9775bf223ea
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-10 14:48:01 -0500

    commit tree 506e8fa810cb1e43a1546fa29ed3227f9030b665

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "Keegan",
+    streetAdress: "100 street",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    console.log(employee, key, value)
+    const newObj = {...employee};
+    newObj[key] = value;
+   
+ 
+    return newObj;
+    }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    console.log(employee, key, value)
+    employee[key] = value;
+    console.log(employee);
+    return employee;
+    }
+
+function deleteFromEmployeeByKey(employee, key){
+    console.log(employee, key);
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    
+    return newEmployee;
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    console.log(employee, key);
+    delete employee[key];
+    return employee;
+    
+}
